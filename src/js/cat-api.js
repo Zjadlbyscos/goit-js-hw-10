c04c7aff90e4547bc0520fe2da42395ce1a04c46
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-09-01 22:49:10 +0200

    Breed list shows up, console logs whith elements, no images yet

---
+++ src/js/cat-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_THhvGlPVf7TmHxqeARMtqpJQU8Py3GxaCsCHrcbtydYuUFyD360SuIUQN7g9Q7tH';
+  
+  const apiUrl = 'https://api.thecatapi.com/v1';
+  //wysyła żądanie (GET DO API) HTTP i zwraca obietnicę z tablicą ras - wynikiem żądania. Za pomocą TRY CATCH 
+
+const fetchBreeds = async () => {
+    try{
+    return axios
+      .get(`${apiUrl}/breeds`)
+      .then(response => {
+        return response.data;
+      });
+    }catch(error) {
+        new Error('Oops! Something went wrong! Try reloading the page!');
+      }};
+  // ---------errrprp zmienic z uzyciem biblioteki. 
+  export{fetchBreeds}
+
---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import {fetchBreeds} from './cat-api';
+const obj = {
+breedSelect: document.querySelector('.breed-select'),
+loader: document.querySelector('.loader'),
+error: document.querySelector('.error'),
+catInfo: document.querySelector('.description')
+
+};
+
+ fetchBreeds().then(breeds=>{
+breeds.forEach((breed)=>{
+  console.log(breed)
+  const option = document.createElement('option');
+  option.value = breed.id; // Ustaw wartość opcji na ID rasy
+  option.textContent = breed.name; // Ustaw tekst opcji na nazwę rasy
+  obj.breedSelect.appendChild(option); // Dodaj opcję do <select>
+});
+
+ })
+ .catch(error => {
+  new Error('Oops! Something went wrong! Try reloading the page!');
+});
+
